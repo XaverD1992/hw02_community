1773c846855255138b5891e0fe7999cbd7adf728
Владислав Суворов <gordik_08@mail.ru>
2022-11-27 17:47:15 +0300

    My tenth commit

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,10 +4,6 @@ from django.db import models
 User = get_user_model()
 
 
-class Meta:
-    ordering = ['-pub_date']
-
-
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
@@ -31,3 +27,6 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
         related_name='posts'
     )
+
+    class Meta:
+        ordering = ['-pub_date']
